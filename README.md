543f07aabce23e61f9b9ace459e605582e9ed79e
ticktick <lujun.hust@gmail.com>
2015-07-24 09:11:31 +0800

    add main.c

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 Linux
 ===========
 
-本目录下的代码均是基于Linux平台的C/C++代码，都是我的编程经验积累，部分代码来自我的博客文章。
+本目录下的代码均是基于Linux平台的C/C++代码。
 
 Code List
 ----------
 
+- main.c 演示了带命令行参数的main函数的使用
 - pipe   封装了Linux系统的无名管道（pipe），可以方便地集成到C语言项目中作为“队列”来使用。
-- syslog 演示如何构建一个“专业”的Log系统，博文：[构建专业的log系统](http://ticktick.blog.51cto.com/823160/1579785)
+- syslog 演示如何构建一个“专业”的Log系统，相关博文：[构建专业的log系统](http://ticktick.blog.51cto.com/823160/1579785)
 
 Thanks
 ----------
---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define LOG(format,...) printf(format,##__VA_ARGS__)
+
+void usage(char *program) {
+    LOG("Usage:\n%s [-i interface [null/eth0/eth1...]] [-p listenport] [-h help] \n",program);
+    LOG("example:\n");
+    LOG("\t%s -i eth0 -p 6000 \n",program);
+}
+
+int main(int argc, char *argv[])
+{
+    int c;
+    char interface[64];
+    int listen_port;
+
+    // argv[0] == program name
+    usage(argv[0]);
+
+    LOG("You have input %d params\n",argc-1); 
+
+    //xyz: 单个选项，例如： -h
+    //x:   如果x存在，则必须跟一个参数(带空格)，例如： -p 6000
+    //x::  如果x存在，则必须跟一个参数（不带空格），例如： -ieth0
+    while ((c = getopt(argc, argv, "i::p:h?")) != -1) {
+        switch (c) 
+        {
+        case 'i':
+            strcpy(interface , optarg);  
+            LOG("interface %s \n",interface);
+            break;
+        case 'p':
+            listen_port = atoi(optarg);
+            LOG("listen_port %d \n",listen_port); 
+            break;
+        case 'h':
+        case '?':
+            LOG("Need help ?\n");
+            break;        
+        default:
+            usage(argv[0]);
+            return 1;
+        }
+    }
+
+    return 0;
+}
