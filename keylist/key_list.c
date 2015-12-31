d67283a0f23757487f8c6b25e3ce1ac154509fdb
ticktick <lujun.hust@gmail.com>
2015-12-31 14:05:35 +0800

    update file headers

--- keylist/key_list.c
+++ keylist/key_list.c
@@ -3,7 +3,7 @@
  *  Copyright (C) 2015, Jhuster, All Rights Reserved
  *  Author:  Jhuster(lujun.hust@gmail.com)
  *  
- *  https://github.com/Jhuster/C-Cpp
+ *  https://github.com/Jhuster/clib
  *   
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
--- thread/thread.h
+++ thread/thread.h
@@ -1,3 +1,15 @@
+/*
+ *  COPYRIGHT NOTICE  
+ *  Copyright (C) 2015, Jhuster, All Rights Reserved
+ *  Author:  Jhuster(lujun.hust@gmail.com)
+ *  
+ *  https://github.com/Jhuster/clib
+ *   
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.  
+ */
+ 
 #ifndef _THREAD_H_
 #define _THREAD_H_
 
